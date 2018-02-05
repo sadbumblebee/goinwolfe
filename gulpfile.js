74c306b94f956c79df8381adfff1d0dc3dede49c
Daniel Wolfe <jdanielwolfe@gmail.com>
2018-02-04 17:50:57 -0800

    woof website going up for edits

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const ifElse = require('gulp-if-else');
 const argv = require('yargs').argv;
 
 gulp.task('images', () => {
-  gulp.src(['source/images/**/*.{jpg,jpeg,png,gif,svg}'], { base: 'source/images' })
+  gulp.src(['source/images/**/*.{jpg,jpeg,png,gif,svg}'], { base: 'docs/images' })
     .pipe(ifElse(argv.compress, () => imagemin()))
     .pipe(gulp.dest('build/images'));
 });
